8021e05c1387662811a4b38a4fa16d1e28a42c6c
Jose M Andres <jmandreslopez@gmail.com>
2017-02-24 12:41:59 -0600

    build(angular): Remove unnecessary angular2 dependencies and downgrade to 2.4.+
    Remove unnecessary angular2 dependencies (CommonModule) and downgrade to 2.4.+ (^2.4.0)

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,14 @@
  */
 
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TrackScrollDirective } from './track-scroll';
 
 @NgModule({
-    imports: [CommonModule],
     declarations: [TrackScrollDirective],
     exports: [TrackScrollDirective]
 })
 export class Ng2TrackScrollModule {
     static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: Ng2TrackScrollModule
-        };
+        return { ngModule: Ng2TrackScrollModule, providers: [] };
     }
 }
